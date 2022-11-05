d53377c1a205dfdfabe41184a8df8fb9b99256f7
Ezio Lin <ninesuns.lin@outlook.com>
2022-11-05 15:54:37 +0800

    ♻️ UI与状态书写分离 (#2)
    * ♻️ UI与状态书写分离
    
    使用 React Hook 将概念一致的状态封装组合，再输出到 UI 的 JSX 部分
    使得代码逻辑更加内聚
    
    * ♻️ 解决"新建功能"引入的冲突并继续分离逻辑与UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { Card, Col, Row, Input, Button, Empty } from "@douyinfe/semi-ui";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { Card, Col, Row, Input, Button, Empty, Toast } from "@douyinfe/semi-ui";
 import "./App.css";
 import { load, type SpaceVo } from "./space";
 
@@ -11,60 +11,122 @@ const chunk = (arr: Array<SpaceVo>, size: number): Array<Array<SpaceVo>> => {
   return ret;
 };
 
-type Pack = {
-  name: string;
-  physicsPath: string;
-};
+const filterMapWhenResponseOk = (res: Response) => {
+  if (res.ok) {
+    return res.json()
+  } else {
+    return Promise.reject(new Error("接口响应失败"))
+  }
+}
 
-function App() {
+const noop = (): void => {}
+const constTrue = () => true
+const constFalse = () => false 
+
+const useRunOnce = <T extends (...a: any[]) => void>(fn: T) => {
+  // 当前版本 trick 用闭包保持fn的值
+  const fnRef  = useRef(fn)
+  fnRef.current = fn
+
+  return useMemo(
+    () => {
+      let shouldRun = true
+      return (...args: Parameters<T>): void => {
+        if (shouldRun) {
+          shouldRun = false
+          fnRef.current(...args)
+        }
+      }
+    },
+    []
+  )
+}
+
+/**
+ * 获得已经定义好的 space 的状态
+ */
+const useSpaces = () => {
   const [spaces, setSpaces] = useState<Array<SpaceVo>>([]);
-  const [editing, setEditing] = useState<string>();
-  const [creating, setCreating] = useState<boolean>(false);
-  const [pack, setPack] = useState<Pack>({ name: "", physicsPath: "" });
 
-  async function init() {
-    const spaces = await load();
-    setSpaces(spaces);
+  const getLastedSpaces = () => {
+    return load().then(setSpaces)
   }
 
-  const doCreate = async () => {
-    await fetch(`api/space`, {
+  const runWhenMount = useRunOnce(() => getLastedSpaces())
+
+  useEffect(() => runWhenMount(), /* run once */ [])
+
+  const [editingId, setEditingId] = useState<string>();
+
+  const modifyInLocal = (id: string) => (fn: (prevData: SpaceVo) => SpaceVo) => {
+    setSpaces((it) => it.map(it => it.id === id ? fn(it) : it))
+  }
+
+  const updateToServer = (data: SpaceVo) => {
+    return fetch(`api/space/${data.id}`, {
+      method: "put",
+      body: JSON.stringify(data),
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then(filterMapWhenResponseOk)
+      .then(getLastedSpaces)
+      .then(constTrue, constFalse)
+
+  }
+
+  return { spaces, getLastedSpaces, modifyInLocal, updateToServer, editingId, setEditingId } 
+}
+
+/**
+ * 创建 Space 所包含信息类型
+ */
+type Pack = Pick<SpaceVo, "name" | "physicsPath">;
+
+/**
+ * 创建新 space
+ */
+const useCreatingSpace = () => {
+  const empty: Pack = { name: "", physicsPath: "" }
+  const [pack, setPack] = useState<Pack | undefined>();
+
+  const startCreating = () => setPack(empty)
+
+  const postToServer = () => {
+    return fetch(`api/space`, {
       method: "post",
       body: JSON.stringify(pack),
       headers: {
         "content-type": "application/json",
       },
-    });
-    setPack({ name: "", physicsPath: "" });
-    setCreating(false);
-    await init();
+    })
+      .then(filterMapWhenResponseOk)
+      .then(() => setPack(undefined))
+      .then(constTrue, constFalse)
+  }
+
+  return { startCreating, pack, setPack, postToServer }
+}
+
+function App() {
+  const { spaces, getLastedSpaces, modifyInLocal, updateToServer, editingId, setEditingId } = useSpaces();
+
+  const { startCreating, pack, setPack, postToServer } = useCreatingSpace()
+
+  const doUpdate = (e: SpaceVo) => {
+      updateToServer(e)
+        .then((ok) => (ok ? noop : Toast.error.bind(Toast))("更新失败"))
+  }
+
+  const doCreate = () => {
+    postToServer()
+      .then((ok) => (ok ? getLastedSpaces : (msg: string) => { Toast.error(msg) })("创建失败"))
   };
 
-  const save = async (ele: SpaceVo) => {
-    await fetch(`api/space/${ele.id}`, {
-      method: "put",
-      body: JSON.stringify(ele),
-      headers: {
-        "content-type": "application/json",
-      },
-    });
-    await init();
+  const modifyPhysicsPathOfExistSpace = (id: string) => (eventValue: string) => {
+    modifyInLocal(id)((space) => ({ ...space, physicsPath: eventValue }))
   };
-
-  const handleChange = (id: string) => (eventValue: string) => {
-    setSpaces(
-      spaces.map((space) => {
-        if (space.id === id) {
-          return { ...space, physicsPath: eventValue };
-        }
-        return space;
-      })
-    );
-  };
-
-  useEffect(() => {
-    init();
-  }, []);
 
   return (
     <div className="App" style={{ width: 1280 }}>
@@ -88,17 +150,17 @@ function App() {
                       <Input
                         value={ele.physicsPath}
                         onFocus={() => {
-                          setEditing(ele.id);
+                          setEditingId(ele.id);
                         }}
-                        onChange={handleChange(ele.id)}
+                        onChange={modifyPhysicsPathOfExistSpace(ele.id)}
                       ></Input>
                       <Row gutter={[8, 8]} style={{ marginTop: 16 }}>
                         <Col span={12}>
                           <Button>view</Button>
                         </Col>
-                        {editing === ele.id && (
+                        {editingId === ele.id && (
                           <Col span={12}>
-                            <Button onClick={() => save(ele)}>save</Button>
+                            <Button onClick={() => doUpdate(ele)}>save</Button>
                           </Col>
                         )}
                       </Row>
@@ -113,7 +175,7 @@ function App() {
         <div>
           <Empty title={"没有空间数据"} description="暂时没有任何的空间" />
           <div style={{ margin: "8px", padding: "6px 24px" }}>
-            {creating && (
+            {pack && (
               <div>
                 <Input
                   placeholder="起个名字吧"
@@ -130,23 +192,19 @@ function App() {
                   style={{ margin: "8px", padding: "6px 24px" }}
                   theme="solid"
                   type="primary"
-                  onClick={() => {
-                    doCreate();
-                  }}
+                  onClick={doCreate}
                 >
                   保存
                 </Button>
               </div>
             )}
-            {!creating && (
+            {!pack && (
               <Button
-                disabled={creating}
+                disabled={!!pack}
                 style={{ margin: "8px", padding: "6px 24px" }}
                 theme="solid"
                 type="primary"
-                onClick={() => {
-                  setCreating(true);
-                }}
+                onClick={startCreating}
               >
                 新建一个
               </Button>
